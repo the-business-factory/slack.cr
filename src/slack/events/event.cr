2b8b09f909e63aaf8c83069db8a52363c25c5d5b
Rob Cole <robcole@gmail.com>
2022-03-10 16:34:28 -0800

    Extract Message Factory from Events
    Mirror Slack's event "structure", allowing a single
    type for new messages, and additional types based
    on the presence of message + its optional subtypes.
    
    Cleans up the interface to make it a little bit more
    self-documenting.

--- src/slack/events/event.cr
+++ src/slack/events/event.cr
@@ -1,13 +1,12 @@
 class Slack::Event
   include JSON::Serializable
-  include Slack::SerializableEvent
 
   property type : String
 
   use_json_discriminator "type", {
     app_home_opened:  Slack::Events::AppHomeOpened,
     app_uninstalled:  Slack::Events::AppUninstalled,
-    message:          Slack::Events::Message,
+    message:          Slack::Events::MessageFactory,
     reaction_added:   Slack::Events::ReactionAdded,
     reaction_removed: Slack::Events::ReactionRemoved,
     tokens_revoked:   Slack::Events::TokensRevoked,
---
+++ src/slack/events/event_types/message_factory.cr
@@ -0,0 +1,12 @@
+class Slack::Events::MessageFactory
+  include Slack::OptionalDiscriminator
+
+  use_optional_discriminator_on "subtype",
+    Slack::Events::Message,
+    {
+      bot_add:         Slack::Events::Message::BotAdd,
+      channel_join:    Slack::Events::Message::ChannelJoin,
+      message_changed: Slack::Events::Message::MessageChanged,
+      message_deleted: Slack::Events::Message::MessageDeleted,
+    }
+end
