7c858208819594ffc44fb871af9257915f6555de
Rob Cole <robcole@gmail.com>
2022-04-18 18:10:44 -0700

    Add chatDelete endpoint support
    Allow bots to delete posts they have access
    to remove.

---
+++ spec/api/chat_delete_spec.cr
@@ -0,0 +1,19 @@
+require "../spec_helper"
+
+describe Slack::Api::ChatDelete do
+  describe "#call" do
+    it "should post a chat message with one block" do
+      token = ENV.fetch("SLACK_TEAM_AUTH_TOKEN")
+      ts = "1650310389.011519"
+
+      load_cassette("chat-delete-success") do
+        response = Slack::Api::ChatDelete
+          .new(token: token, channel: "C03B5PUPDSQ", ts: ts)
+          .call
+          .should be_a(Slack::Models::Chat::Delete)
+
+        response.ts.should eq ts
+      end
+    end
+  end
+end
