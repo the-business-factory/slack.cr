e3547381f98a090f6bea0d6a5d7910fafa2ac655
Rob Cole <robcole@gmail.com>
2022-04-22 17:11:02 -0700

    Allow messages to determine if they are from a thread

--- src/slack/events/event_types/message_subtype.cr
+++ src/slack/events/event_types/message_subtype.cr
@@ -3,7 +3,12 @@ class Slack::Events::MessageSubtype < Slack::Event
     channel_type : String,
     subtype : String,
     event_ts : String,
+    thread_ts : String?,
     ts : String
+
+  def thread?
+    thread_ts.present?
+  end
 
   def public_channel?
     channel_type == "channel"
