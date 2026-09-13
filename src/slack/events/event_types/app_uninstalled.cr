357a356505c757bebb0fc41f595b9d469e74628d
Rob Cole <robcole@gmail.com>
2026-09-12 21:22:37 -0700

    Process verified credential revocation and uninstall with installation fences

--- src/slack/events/event_types/app_uninstalled.cr
+++ src/slack/events/event_types/app_uninstalled.cr
@@ -1,2 +1,4 @@
 struct Slack::Events::AppUninstalled < Slack::Event
+  @[JSON::Field(emit_null: false)]
+  property event_ts : String?
 end
--- src/slack/events/event_types/tokens_revoked.cr
+++ src/slack/events/event_types/tokens_revoked.cr
@@ -1,3 +1,14 @@
 struct Slack::Events::TokensRevoked < Slack::Event
-  property tokens : JSON::Any, event_ts : String
+  # Slack sends user IDs, including bot user IDs, never token strings.
+  struct Tokens
+    include JSON::Serializable
+
+    getter oauth : Array(String) = [] of String
+    getter bot : Array(String) = [] of String
+  end
+
+  property tokens : Tokens
+
+  @[JSON::Field(emit_null: false)]
+  property event_ts : String?
 end
